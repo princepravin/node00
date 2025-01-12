147b905efa73d489cb15d0c2a2c4e0cbbf46e89a
honerise <honerise@honerises-MacBook-Air.local>
2025-01-12 13:28:22 +0530

    just pushed my sql connection

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const router = require('./routerFile');
 const express = require('express');
 // setupDb();
 
+const sql = require('mysql2');
+const mysql2 = sql.createPool({
+    host:"localhost",
+    user:"",
+    password:"",
+    database:"Address"
+})
+
+
+mysql2.query("Select * from Address",(err,res)=>{
+    console.log(err)
+    console.log(res)
+})
 const app = express();
 app.use(express.json());
 const cors = require('cors')
